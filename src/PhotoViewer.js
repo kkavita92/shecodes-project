15d58751ab0c733292926f207a821d3ca4351160
kkavita92 <kkavita92@gmail.com>
2017-11-25 13:47:29 +0000

    Transfer image styling to CSS file

--- src/PhotoViewer.js
+++ src/PhotoViewer.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
+import './PhotoViewer.css';
 
 class PhotoViewer extends Component {
   constructor(props) {
@@ -7,17 +8,17 @@ class PhotoViewer extends Component {
     this.state = {
       imageURL: 'https://images.unsplash.com/photo-1493246507139-91e8fad9978e?auto=format&fit=crop&w=1950&q=60&ixid=dW5zcGxhc2guY29tOzs7Ozs%3D'
     }
-  }
+  }a
 
   render() {
     let imagePreview = null;
 
     if (this.state.imageURL) {
-      imagePreview = (<img src={this.state.imageURL} />);
+      imagePreview = (<img src={this.state.imageURL} className="image-viewer" alt="" />);
     }
 
     return (
-      <div className="viewer">
+      <div className="image-container">
         {imagePreview}
       </div>
     );
