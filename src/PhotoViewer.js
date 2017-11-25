9852686a7b39b4d85d08df5f29e0f4837cf7b11e
kkavita92 <kkavita92@gmail.com>
2017-11-25 13:17:39 +0000

    Create PhotoViewer component and pull into App

---
+++ src/PhotoViewer.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class PhotoViewer extends Component {
+  render() {
+
+    return (
+      <div className="Viewer">
+        <header className="App-header">
+          <h1 className="App-title">Welcome to React</h1>
+        </header>
+        <p className="App-intro">
+          To get started, edit <code>src/App.js</code> and save to reload.
+        </p>
+      </div>
+    );
+  }
+}
+
+export default PhotoViewer;
